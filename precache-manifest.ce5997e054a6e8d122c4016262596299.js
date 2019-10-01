c7a2a9a454fc264008d4c298edff9e00536bc98a
Dabeer <dabeerofficial@gmail.com>
2019-10-01 15:40:35 +0500

    Updates

---
+++ precache-manifest.ce5997e054a6e8d122c4016262596299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd90f7fe08b0ce6b11f43dfe01bdcb4",
+    "url": "/online-article-app/index.html"
+  },
+  {
+    "revision": "e3be0940fd948c4c5d41",
+    "url": "/online-article-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "68754d90a6e92fa8225d",
+    "url": "/online-article-app/static/js/2.c56e2d42.chunk.js"
+  },
+  {
+    "revision": "e3be0940fd948c4c5d41",
+    "url": "/online-article-app/static/js/main.d60307a1.chunk.js"
+  },
+  {
+    "revision": "1bec539e6ee3fe25510d",
+    "url": "/online-article-app/static/js/runtime-main.c95f2d00.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/online-article-app/static/media/logo.25bf045c.svg"
+  }
+]);
